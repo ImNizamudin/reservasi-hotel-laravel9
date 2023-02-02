7308afa092b9163127a524a79aa1b978dedf23bf
MuhWijdanandiPratama <pratamwijdanandi@gmail.com>
2023-02-02 09:22:15 +0700

    layouting resepsionis button search

--- resources/views/dashboard/resepsionis/index.blade.php
+++ resources/views/dashboard/resepsionis/index.blade.php
@@ -46,21 +46,27 @@
     }
 </style>
 
-<div style="margin-top: -80px; margin-left: 30px;" class="group">
-    <svg class="icon" aria-hidden="true" viewBox="0 0 24 24">
-        <g>
-            <path
-                d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
-            </path>
-        </g>
-    </svg>
-    <input style="width: 300px;" placeholder="Search" type="search" class="input">
+<div class="row">
+    <div class="col-3">
+        <div style="margin-left: 30px;"
+            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Kode Booking</h1>
+        </div>
+    </div>
+    <div class="col-9">
+        <div style="margin-top: 20px; margin-left: 30px;" class="group">
+            <svg class="icon" aria-hidden="true" viewBox="0 0 24 24">
+                <g>
+                    <path
+                        d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                    </path>
+                </g>
+            </svg>
+            <input style="width: 300px;" placeholder="Search" type="search" class="input">
+        </div>
+    </div>
 </div>
 
-<div style="margin-left: 30px;"
-    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Kode Booking</h1>
-</div>
 
 @if (session()->has('success'))
 <div class="alert alert-success alert-dismissible fade show col-lg-6" role="alert">
